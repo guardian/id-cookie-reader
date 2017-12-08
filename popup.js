b7682cbe1904c80e843a040c1451c4ae7f686037
Laura <laura.gonzalez@guardian.co.uk>
2017-12-08 17:04:38 +0000

    add final touches

--- popup.js
+++ popup.js
@@ -1,112 +1,60 @@
-// Copyright (c) 2014 The Chromium Authors. All rights reserved.
-// Use of this source code is governed by a BSD-style license that can be
-// found in the LICENSE file.
+const getCurrentTabUrl = async () => {
 
-/**
- * Get the current URL.
- *
- * @param {function(string)} callback called when the URL of the current tab
- *   is found.
- */
-function getCurrentTabUrl(callback) {
-  // Query filter to be passed to chrome.tabs.query - see
-  // https://developer.chrome.com/extensions/tabs#method-query
-  var queryInfo = {
+  const queryInfo = {
     active: true,
     currentWindow: true
   };
 
-  chrome.tabs.query(queryInfo, (tabs) => {
-    // chrome.tabs.query invokes the callback with a list of tabs that match the
-    // query. When the popup is opened, there is certainly a window and at least
-    // one tab, so we can safely assume that |tabs| is a non-empty array.
-    // A window can only have one active tab at a time, so the array consists of
-    // exactly one tab.
-    var tab = tabs[0];
-
-    // A tab is a plain object that provides information about the tab.
-    // See https://developer.chrome.com/extensions/tabs#type-Tab
-    var url = tab.url;
-
-    // tab.url is only available if the "activeTab" permission is declared.
-    // If you want to see the URL of other tabs (e.g. after removing active:true
-    // from |queryInfo|), then the "tabs" permission is required to see their
-    // "url" properties.
-    console.assert(typeof url == 'string', 'tab.url should be a string');
-
-    callback(url);
+  return new Promise((yay,nay)=>{
+    chrome.tabs.query(queryInfo, (tabs) =>
+      yay(tabs[0].url)
+    );
   });
 
-  // Most methods of the Chrome extension APIs are asynchronous. This means that
-  // you CANNOT do something like this:
-  //
-  // var url;
-  // chrome.tabs.query(queryInfo, (tabs) => {
-  //   url = tabs[0].url;
-  // });
-  // alert(url); // Shows "undefined", because chrome.tabs.query is async.
 }
 
-/**
- * Change the background color of the current page.
- *
- * @param {string} color The new background color.
- */
-function changeBackgroundColor(color) {
-  var script = 'document.body.style.backgroundColor="' + color + '";';
-  // See https://developer.chrome.com/extensions/tabs#method-executeScript.
-  // chrome.tabs.executeScript allows us to programmatically inject JavaScript
-  // into a page. Since we omit the optional first argument "tabId", the script
-  // is inserted into the active tab of the current window, which serves as the
-  // default.
-  chrome.tabs.executeScript({
-    code: script
+const getCookieAsString = (cookie) => {
+  try {
+    return atob(cookie.value.split('.')[0])
+  } catch(e) {
+    return undefined;
+  }
+}
+
+const getCookieAsJSON = (cookie) => {
+  try {
+    return JSON.parse(cookie).filter(txt => txt.toString().length > 0);
+  } catch(e) {
+    return undefined;
+  }
+}
+
+const action = async () => {
+
+  const url = await getCurrentTabUrl();
+
+  chrome.cookies.get({
+    url,
+    name: 'GU_U',
+  },(cookie)=>{
+
+    const decodedCookieAsString = getCookieAsString(cookie);
+    const decodedCookieAsJSON = getCookieAsJSON(decodedCookieAsString);
+
+    if(typeof decodedCookieAsJSON !== 'undefined') {
+      document.querySelector('section').innerHTML = decodedCookieAsJSON.join('<hr/>')
+    }
+    else if (typeof decodedCookieAsString !== 'undefined') {
+      document.querySelector('section').innerHTML = decodedCookieAsString
+    }
+    else {
+      document.querySelector('section').classList.add('error')
+      document.querySelector('section').innerHTML = 'is GU_U set?';
+    }
   });
+
 }
 
-/**
- * Gets the saved background color for url.
- *
- * @param {string} url URL whose background color is to be retrieved.
- * @param {function(string)} callback called with the saved background color for
- *     the given url on success, or a falsy value if no color is retrieved.
- */
-function getSavedBackgroundColor(url, callback) {
-  // See https://developer.chrome.com/apps/storage#type-StorageArea. We check
-  // for chrome.runtime.lastError to ensure correctness even when the API call
-  // fails.
-  chrome.storage.sync.get(url, (items) => {
-    callback(chrome.runtime.lastError ? null : items[url]);
-  });
-}
-
-/**
- * Sets the given background color for url.
- *
- * @param {string} url URL for which background color is to be saved.
- * @param {string} color The background color to be saved.
- */
-function saveBackgroundColor(url, color) {
-  var items = {};
-  items[url] = color;
-  // See https://developer.chrome.com/apps/storage#type-StorageArea. We omit the
-  // optional callback since we don't need to perform any action once the
-  // background color is saved.
-  chrome.storage.sync.set(items);
-}
-
-document.addEventListener('DOMContentLoaded', () => {
-
-  getCurrentTabUrl((url) => {
-
-    chrome.cookies.get({
-      url,
-      name: 'GU_U',
-    },cookie=>{
-      console.log(cookie);
-      document.querySelector('section').innerHTML = (
-        atob(cookie.value.split('.')[0])
-      )
-    });
-  });
+document.addEventListener('DOMContentLoaded', async () => {
+  action();
 });
